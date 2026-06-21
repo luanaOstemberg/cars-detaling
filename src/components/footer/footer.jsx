583e5d5ba59c3c62d17e2837a3a0cd258a9cf3ff
luanaostemberg <ostembergluana98@gmail.com>
2026-06-21 02:43:06 -0300

    se agrega el footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,15 @@
-export default function Footer (){
+import { Link } from "react-router";
+import styles from "./styles.module.css"
+export default function Footer() {
     return (
-        <h1>Footer</h1>
+        <footer className={styles.footer}>
+            <div className={styles.container__links}>
+            
+            <Link className={styles.container__link} target="_blank" to={"https://wa.me/5491140634484"}><i class={`bi bi-whatsapp`}></i>
+                WhatsApp</Link>
+            <Link className={styles.container__link} target="_blank" to={"https://www.instagram.com/vgesteticavehicular"}><i class={` bi bi-instagram`}></i>Instagram</Link>
+            </div>
+            <p className={styles.text__copy}>&copy; 2026 Todos los derechos reservados.</p>
+        </footer>
     )
 }
--- src/presentation/home/home.jsx
+++ src/presentation/home/home.jsx
@@ -1,5 +1,5 @@
 export default function Home() {
     return (
-        <h1>Hola Mundo</h1>
+        <main style={{ "background": "black" }}>Hola Mundo</main>
     )
 }
