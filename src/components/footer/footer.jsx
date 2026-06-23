af9dff253f47dab9ad85dc55ca772c11f3fa9c6d
luanaostemberg <ostembergluana98@gmail.com>
2026-06-23 18:35:17 -0300

    v1.1

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
             <div className={styles.container__links}>
             
             <Link className={styles.container__link} target="_blank" to={"https://wa.me/5491140634484"}><i class={`bi bi-whatsapp`}></i>
-                WhatsAp</Link>
+                WhatsApp</Link>
             <Link className={styles.container__link} target="_blank" to={"https://www.instagram.com/vgesteticavehicular"}><i class={` bi bi-instagram`}></i>Instagram</Link>
             </div>
             <p className={styles.text__copy}>&copy; 2026 Todos los derechos reservados.</p>
